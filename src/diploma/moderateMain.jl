817577a86ee3d95568706600f8abc10de0e03eb1
Vedblade <18503283+yad439@users.noreply.github.com>
2020-10-06 14:42:11 +0700

    Experiment annealing and genetic

--- src/diploma/moderateMain.jl
+++ src/diploma/moderateMain.jl
@@ -1,18 +1,45 @@
 include("$(@__DIR__)/moderateLinear.jl")
 include("$(@__DIR__)/modularLocal.jl")
-include("$(@__DIR__)/auxiliary.jl.jl")
+include("$(@__DIR__)/modularAnnealing.jl")
+include("$(@__DIR__)/modularGenetic.jl")
+include("$(@__DIR__)/auxiliary.jl")
 
 using Plots
 using Printf
 using Statistics
 
-n=5
+n=7
 m=2
 p=rand(5:20,n)
 k=rand(1:2,n)
 tt=20
 c=4
 
+scoreFun=jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt)
+
 exactRes=moderateExact(n,m,c,p,k,tt)
-localRes1=modularTabuSearch(n,m,TabuSearchSettings(100,10,10),jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt),randomTwoVectorEncoding(n,m))
-localRes2=modularTabuSearch(n,m,TabuSearchSettings(100,10,10),jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt),randomPermutationEncoding(n))
+localRes1=modularTabuSearch(n,m,TabuSearchSettings(100,10,10),scoreFun,randomTwoVectorEncoding(n,m))
+localRes2=modularTabuSearch(n,m,TabuSearchSettings(100,10,10),scoreFun,randomPermutationEncoding(n))
+tmp=randomPermutationEncoding(n)
+annealingRes1=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
+tmp=randomTwoVectorEncoding(n,m)
+annealingRes2=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
+tmp=randomPermutationEncoding(n)
+annealingRes3=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+tmp=randomTwoVectorEncoding(n,m)
+annealingRes4=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+popul=[begin
+	val=randomPermutationEncoding(n)
+	score=scoreFun(val)
+	GeneticEntity(val,score)
+end for _=1:10] |> sort!
+crossw=(p1,p2)->PermutationEncoding(pmxCrossover(p1.permutation,p2.permutation))
+mutate(val)=val->rand()>0.5 && randomChange!(val,it->true,n,m)
+geneticRes1=modularGenetic(n,m,GeneticSettings(100,2,crossw,mutate),scoreFun,popul)
+popul=[begin
+	val=randomTwoVectorEncoding(n,m)
+	score=scoreFun(val)
+	GeneticEntity(val,score)
+end for _=1:10] |> sort!
+crossw=(p1,p2)->TwoVectorEncoding(elementviseCrossover(p1.assignment,p2.assignment),pmxCrossover(p1.permutation,p2.permutation))
+geneticRes2=modularGenetic(n,m,GeneticSettings(100,2,crossw,mutate),scoreFun,popul)
