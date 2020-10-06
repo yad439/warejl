c4ea3a0c9bf37de6b37736cc2200f505022ba789
Vedblade <18503283+yad439@users.noreply.github.com>
2020-10-06 11:43:44 +0700

    Experiment modular medium

--- src/diploma/mediumMain.jl
+++ src/diploma/mediumMain.jl
@@ -1,5 +1,6 @@
 include("$(@__DIR__)/simpleLinear.jl")
 include("$(@__DIR__)/mediumLocal.jl")
+include("$(@__DIR__)/modularLocal.jl")
 
 using Plots
 using Printf
@@ -14,15 +15,23 @@ p=rand(1:100,n)
 exactSol=exact(n,m,p)
 localRes1=localTabu2(n,m,p,TabuSearchSettings(100,10,100))
 localRes2=localTabu3(n,m,p,TabuSearchSettings(100,10,100))
+localRes3=modularTabuSearch(n,m,TabuSearchSettings(100,10,100),jobs->maxTime(jobs,p,m),randomPermutationEncoding(n))
+localRes4=modularTabuSearch(n,m,TabuSearchSettings(100,10,100),jobs->maxTime(jobs,p,m),randomTwoVectorEncoding(n,m))
 
 @printf("Local tabu (permutation): %f%%\n",100(localRes1[2]/exactSol[2])-100)
 @printf("Local tabu (double): %f%%\n",100(localRes2[2]/exactSol[2])-100)
+@printf("Local tabu (permutation): %f%%\n",100(localRes3[2]/exactSol[2])-100)
+@printf("Local tabu (double): %f%%\n",100(localRes4[2]/exactSol[2])-100)
 
 plot(localRes1[3])
 plot(localRes2[3])
+plot(localRes3[3])
+plot(localRes4[3])
 
 plot(neededCarCountHistory(computeTimesOfPermutation(localRes1[1],p,m),50))
 plot(neededCarCountHistory(computeTimes(localRes2[1],p,m),50))
+plot(neededCarCountHistory(computeTimes(localRes3[1],p,m),50))
+plot(neededCarCountHistory(computeTimes(localRes4[1],p,m),50))
 
 rep=map(1:10) do _
 	pl=rand(n)*10
