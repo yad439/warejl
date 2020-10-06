2ae48f1859c0ad2ae8c6da4de58a8c7b45ab884f
Vedblade <18503283+yad439@users.noreply.github.com>
2020-10-06 16:36:17 +0700

    Experiment moderate big numbers

--- src/diploma/moderateMain.jl
+++ src/diploma/moderateMain.jl
@@ -8,38 +8,38 @@ using Plots
 using Printf
 using Statistics
 
-n=7
-m=2
+n=200
+m=5
 p=rand(5:20,n)
 k=rand(1:2,n)
 tt=20
-c=4
+c=10
 
 scoreFun=jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt)
 
-exactRes=moderateExact(n,m,c,p,k,tt)
-localRes1=modularTabuSearch(n,m,TabuSearchSettings(100,10,10),scoreFun,randomTwoVectorEncoding(n,m))
-localRes2=modularTabuSearch(n,m,TabuSearchSettings(100,10,10),scoreFun,randomPermutationEncoding(n))
+exactRes=moderateExact(n,m,c,p,k,tt,300)
+localRes1=modularTabuSearch(n,m,TabuSearchSettings(400,200,200),scoreFun,randomTwoVectorEncoding(n,m))
+localRes2=modularTabuSearch(n,m,TabuSearchSettings(400,200,200),scoreFun,randomPermutationEncoding(n))
 tmp=randomPermutationEncoding(n)
-annealingRes1=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
+annealingRes1=modularAnnealing(n,m,AnnealingSettings(1000,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
 tmp=randomTwoVectorEncoding(n,m)
-annealingRes2=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
+annealingRes2=modularAnnealing(n,m,AnnealingSettings(1000,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
 tmp=randomPermutationEncoding(n)
-annealingRes3=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+annealingRes3=modularAnnealing(n,m,AnnealingSettings(1000,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
 tmp=randomTwoVectorEncoding(n,m)
-annealingRes4=modularAnnealing(n,m,AnnealingSettings(100,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+annealingRes4=modularAnnealing(n,m,AnnealingSettings(1000,maxDif(tmp,n,m,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
 popul=[begin
 	val=randomPermutationEncoding(n)
 	score=scoreFun(val)
 	GeneticEntity(val,score)
-end for _=1:10] |> sort!
+end for _=1:100] |> sort!
 crossw=(p1,p2)->PermutationEncoding(pmxCrossover(p1.permutation,p2.permutation))
-mutate(val)=val->rand()>0.5 && randomChange!(val,it->true,n,m)
-geneticRes1=modularGenetic(n,m,GeneticSettings(100,2,crossw,mutate),scoreFun,popul)
+mutate(val)=val->rand()>0.2 && randomChange!(val,it->true,n,m)
+geneticRes1=modularGenetic(n,m,GeneticSettings(1000,2,crossw,mutate),scoreFun,popul)
 popul=[begin
 	val=randomTwoVectorEncoding(n,m)
 	score=scoreFun(val)
 	GeneticEntity(val,score)
-end for _=1:10] |> sort!
+end for _=1:100] |> sort!
 crossw=(p1,p2)->TwoVectorEncoding(elementviseCrossover(p1.assignment,p2.assignment),pmxCrossover(p1.permutation,p2.permutation))
-geneticRes2=modularGenetic(n,m,GeneticSettings(100,2,crossw,mutate),scoreFun,popul)
+geneticRes2=modularGenetic(n,m,GeneticSettings(1000,2,crossw,mutate),scoreFun,popul)
