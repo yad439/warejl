db94523fb6a98bf42dd5eacece7dbc5742c9823f
Vedblade <18503283+yad439@users.noreply.github.com>
2020-10-10 21:30:00 +0700

    Experiment optimized moderate linear

--- src/diploma/moderateMain.jl
+++ src/diploma/moderateMain.jl
@@ -8,16 +8,18 @@ using Plots
 using Printf
 using Statistics
 
-n=100
+n=10
 m=5
 p=rand(5:20,n)
 k=rand(1:2,n)
-tt=20
-c=10
+tt=10
+c=3
 
 scoreFun=jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt)
+scoreFun2=jobs->maxTimeWithCars(jobs,p,k,m,c,tt)
 
 exactRes=moderateExact(n,m,c,p,k,tt,300)
+exactRes2=moderateExact2(n,m,c,p,k,tt,300)
 localRes1=modularTabuSearch(n,m,TabuSearchSettings(100,100,100),scoreFun,randomTwoVectorEncoding(n,m))
 localRes2=modularTabuSearch(n,m,TabuSearchSettings(100,100,100),scoreFun,randomPermutationEncoding(n))
 localRes3=modularTabuSearch(n,m,TabuSearchSettings2(100,100,0.1),scoreFun,randomTwoVectorEncoding(n,m))
