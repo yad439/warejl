83d20d562cd8bbf44b930d6b55504dc556bf0d69
Vedblade <18503283+yad439@users.noreply.github.com>
2020-10-17 20:59:29 +0700

    Add permutation distance functions

--- src/diploma/utility.jl
+++ src/diploma/utility.jl
@@ -1,5 +1,6 @@
 using Random
 import Base.iterate,Base.eltype
+using OffsetArrays
 
 function randchoice(list,count)
 	notChosen=BitSet(1:length(list))
@@ -40,3 +41,60 @@ function iterate(iter::IteratorSum{T1,T2},state) where {T1,T2}
 	end
 end
 eltype(iter::IteratorSum{T1,T2}) where {T1,T2}=eltype(iter.iter1)
+
+function damerauLevenshteinDistanceOSA(perm1,perm2)
+	len=length(perm1)
+	@assert length(perm2)==len
+	d=OffsetMatrix(Matrix{Int}(undef,len+1,len+1),0:len,0:len)
+	d[:,0]=0:len
+	d[0,:]=0:len
+	for i ∈ eachindex(perm1), j ∈ eachindex(perm2)
+		cost=Int(perm1[i]≠perm2[j])
+		d[i,j]=min(
+			d[i-1,j] + 1, # deletion
+			d[i,j-1] + 1, # insertion
+			d[i-1,j-1] + cost, # substitution
+		)
+		if i>1 && j>1 && perm1[i]==perm2[j-1] && perm1[i-1]==perm2[j]
+			d[i, j]=min(d[i, j],d[i-2, j-2] + 1)# transposition
+		end
+	end
+	d[len,len]
+end
+
+function damerauLevenshteinDistance(a,b)
+	n=length(a)
+	@assert length(b)==n
+
+	da=zeros(Int,n)
+	d=OffsetMatrix(Matrix{Float64}(undef,n+2,n+2),-1:n,-1:n)
+	
+	maxdist = 2n
+	d[-1, -1]=maxdist
+	for i=0:n
+		d[i, -1]=maxdist
+		d[i, 0]=i/2
+		d[-1, i]=maxdist
+		d[0, i]=i/2
+	end
+	
+	for i=1:n
+		db = 0
+		for j=1:n
+			k = da[b[j]]
+			l = db
+			if a[i] == b[j]
+				cost = 0
+				db = j
+			else
+				cost = 0.5
+			end
+			d[i, j] = min(d[i-1, j-1] + cost,#substitution
+							d[i,   j-1] + 0.5,#insertion
+							d[i-1, j  ] + 0.5,#deletion
+							d[k-1, l-1] + (i-k-1) + 1 + (j-l-1))#transposition
+		end
+		da[a[i]]=i
+	end
+	d[n, n]
+end
