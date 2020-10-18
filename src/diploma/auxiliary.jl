adae59c75fe33cafa03f3d9446e54340a9d1bfd1
Vedblade <18503283+yad439@users.noreply.github.com>
2020-10-18 18:04:57 +0700

    Add gantt diagram recipe

--- src/diploma/auxiliary.jl
+++ src/diploma/auxiliary.jl
@@ -1,4 +1,5 @@
 using DataStructures
+using Plots
 
 include("common.jl")
 
@@ -218,4 +219,20 @@ end
 struct Schedule
 	assignment::Vector{Int}
 	times::Vector{Int}
+end
+
+@recipe function scheduleToGantt(jobs::Schedule,jobLengths)
+	n=length(jobs.assignment)
+	@assert length(jobs.times)==n
+	@assert length(jobLengths)==n
+	shapes=[
+		Shape([
+			(jobs.times[i],jobs.assignment[i]-1),
+			(jobs.times[i],jobs.assignment[i]),
+			(jobs.times[i]+jobLengths[i],jobs.assignment[i]),
+			(jobs.times[i]+jobLengths[i],jobs.assignment[i]-1)
+		])
+	for i=1:n]
+	#label:=["job $i" for i=1:n]
+	shapes
 end
