848ab51a8d95e471ceb6925813439e3651b475a5
Vedblade <18503283+yad439@users.noreply.github.com>
2020-10-27 17:48:56 +0700

    Add local search file

---
+++ src/diploma/modularLocal.jl
@@ -0,0 +1,24 @@
+include("utility.jl")
+
+function modularLocalSearch(jobCount,machineCount,settings,scoreFunction,startTimeTable)
+	timeTable=startTimeTable
+	score=scoreFunction(timeTable)
+
+	while true
+		minScore=score
+		minChange=(0,0,0)
+		for change ∈ changeIterator(timeTable,jobCount,machineCount)
+			restore=change!(timeTable,change)
+			val=scoreFunction(timeTable)
+			if val<minScore
+				minScore=val
+				minChange=change
+			end
+			change!(timeTable,restore)
+		end
+		minScore==score && break
+		change!(timeTable,minChange)
+		score=minScore
+	end
+	score,timeTable
+end
