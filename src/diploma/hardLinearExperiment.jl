43777a30651eda5a307b7aaa5606e28ad648f7b9
Vedblade <18503283+yad439@users.noreply.github.com>
2020-11-09 19:43:30 +0700

    Improve hard linear model by linearizing

--- src/diploma/hardLinearExperiment.jl
+++ src/diploma/hardLinearExperiment.jl
@@ -2,18 +2,18 @@ using JuMP,Gurobi
 using LinearAlgebra,Random
 using Plots
 
-n=7
+n=8
 m=3
 p=rand(5:20,n)
-itemCount=5
-itemsNeeded=[randsubseq(1:itemCount,0.5) for _=1:n]
+itemCount=10
+itemsNeeded=[randsubseq(1:itemCount,0.2) for _=1:n]
 travelTime=40
-carNum=6
-storageSize=10
+carNum=4
+storageSize=4
 
 itemsNeededMatrix=[item ∈ itemsNeeded[job] for job=1:n,item=1:itemCount]
 
-T=ceil(Int,sum(length.(itemsNeeded))/carNum)
+T=2ceil(Int,sum(length.(itemsNeeded))/carNum)
 M=sum(p)+T*travelTime
 
 model=Model(Gurobi.Optimizer)
@@ -36,7 +36,15 @@ model=Model(Gurobi.Optimizer)
 @constraint(model,[i=1:n,c=1:carNum,τ=2:2:T],t[i]≤timeSlotTime[c,τ]-1+M*done[i,c,τ])
 @constraint(model,[i=1:n,c=1:carNum,τ=2:2:T],t[i]≥timeSlotTime[c,τ]-M*(1-done[i,c,τ]))
 
-@constraint(model,[i=1:n,item in itemsNeeded[i]],sum((-1)^(τ+1)*timeSlotItem[c,τ,item]*done[i,c,τ] for c=1:carNum,τ=1:T)≥1)
+@variable(model,doneItem[i=1:n,1:carNum,1:T,itemsNeeded[i]],Bin)
+@constraints(model,begin
+	[i=1:n,c=1:carNum,τ=1:T,item in itemsNeeded[i]],doneItem[i,c,τ,item]≤timeSlotItem[c,τ,item]
+	[i=1:n,c=1:carNum,τ=1:T,item in itemsNeeded[i]],doneItem[i,c,τ,item]≤done[i,c,τ]
+	[i=1:n,c=1:carNum,τ=1:T,item in itemsNeeded[i]],doneItem[i,c,τ,item]≥timeSlotItem[c,τ,item]+done[i,c,τ]-1
+end)
+@constraint(model,[i=1:n,item in itemsNeeded[i]],sum((-1)^(τ+1)*doneItem[i,c,τ,item] for c=1:carNum,τ=1:T)≥1)
+
+# @constraint(model,[i=1:n,item in itemsNeeded[i]],sum((-1)^(τ+1)*timeSlotItem[c,τ,item]*done[i,c,τ] for c=1:carNum,τ=1:T)≥1)
 @constraint(model,[τ0=1:T],sum((-1)^(τ+1)*timeSlotItem[c,τ,item] for c=1:carNum,τ=1:τ0,item=1:itemCount)≤storageSize)
 @constraint(model,[τ0=1:T,item=1:itemCount],sum((-1)^(τ+1)*timeSlotItem[c,τ,item] for c=1:carNum,τ=1:τ0)≥0)
 
