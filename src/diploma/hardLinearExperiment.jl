7391a884fc57df303b52f0c4b2d2489210315515
Vedblade <18503283+yad439@users.noreply.github.com>
2020-11-09 14:30:26 +0700

    Add experimental hard linear model

---
+++ src/diploma/hardLinearExperiment.jl
@@ -0,0 +1,47 @@
+using JuMP,Gurobi
+using LinearAlgebra,Random
+using Plots
+
+n=7
+m=3
+p=rand(5:20,n)
+itemCount=5
+itemsNeeded=[randsubseq(1:itemCount,0.5) for _=1:n]
+travelTime=40
+carNum=6
+storageSize=10
+
+itemsNeededMatrix=[item ∈ itemsNeeded[job] for job=1:n,item=1:itemCount]
+
+T=ceil(Int,sum(length.(itemsNeeded))/carNum)
+M=sum(p)+T*travelTime
+
+model=Model(Gurobi.Optimizer)
+@variable(model,t[1:n]≥0)
+
+@variable(model,ord[1:n,1:n],Bin)
+@constraint(model,[i=1:n,j=1:n],t[i]≥t[j]+p[j]-M*(1-ord[j,i]))
+@variable(model,first[1:n],Bin)
+@constraint(model,[i=1:n],sum(ord[:,i])≥1-first[i])
+@constraint(model,[i=1:n,j=1:n,k=1:n; i≠j],ord[i,j]+ord[j,i]≥ord[k,i]+ord[k,j]-1)
+@constraint(model,sum(first)≤m)
+
+@variable(model,timeSlotItem[1:carNum,1:T,1:itemCount],Bin)
+@variable(model,timeSlotTime[1:carNum,1:T]≥0)
+@constraint(model,[c=1:carNum,τ=1:T],sum(timeSlotItem[c,τ,:])≤1)
+@constraint(model,[c=1:carNum,τ=1:T-1],timeSlotTime[c,τ]+travelTime≤timeSlotTime[c,τ+1])
+@variable(model,done[1:n,1:carNum,1:T],Bin)
+@constraint(model,[i=1:n,c=1:carNum,τ=1:2:T],t[i]≤timeSlotTime[c,τ]+travelTime-1+M*done[i,c,τ])
+@constraint(model,[i=1:n,c=1:carNum,τ=1:2:T],t[i]≥timeSlotTime[c,τ]+travelTime-M*(1-done[i,c,τ]))
+@constraint(model,[i=1:n,c=1:carNum,τ=2:2:T],t[i]≤timeSlotTime[c,τ]-1+M*done[i,c,τ])
+@constraint(model,[i=1:n,c=1:carNum,τ=2:2:T],t[i]≥timeSlotTime[c,τ]-M*(1-done[i,c,τ]))
+
+@constraint(model,[i=1:n,item in itemsNeeded[i]],sum((-1)^(τ+1)*timeSlotItem[c,τ,item]*done[i,c,τ] for c=1:carNum,τ=1:T)≥1)
+@constraint(model,[τ0=1:T],sum((-1)^(τ+1)*timeSlotItem[c,τ,item] for c=1:carNum,τ=1:τ0,item=1:itemCount)≤storageSize)
+@constraint(model,[τ0=1:T,item=1:itemCount],sum((-1)^(τ+1)*timeSlotItem[c,τ,item] for c=1:carNum,τ=1:τ0)≥0)
+
+@variable(model,res)
+@constraint(model,[i=1:n],res≥t[i]+p[i])
+@objective(model,Min,res)
+
+optimize!(model)
