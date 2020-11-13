37d00fb5a5864bf947e4214709c579db84a97c80
Vedblade <18503283+yad439@users.noreply.github.com>
2020-11-13 13:43:55 +0700

    Experiment

--- src/diploma/moderateMain.jl
+++ src/diploma/moderateMain.jl
@@ -9,12 +9,12 @@ using Plots
 using Printf
 using Statistics
 ##
-n=11
+n=10
 m=3
 p=rand(5:20,n)
-k=rand(1:2,n)
+k=rand(1:3,n)
 tt=10
-c=3
+c=4
 
 # scoreFun=jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt)
 scoreFun=jobs->maxTimeWithCars(jobs,p,k,m,c,tt)
@@ -23,6 +23,7 @@ exactRes=moderateExact(n,m,c,p,k,tt,300)
 exactRes2=moderateExact2(n,m,c,p,k,tt,300)
 localRes1=modularLocalSearch(LocalSearchSettings(changeIterator(randomTwoVectorEncoding(n,m)),false),scoreFun,randomTwoVectorEncoding(n,m))
 localRes2=modularLocalSearch(LocalSearchSettings(randomChangeIterator(randomTwoVectorEncoding(n,m),100),false),scoreFun,randomTwoVectorEncoding(n,m))
+localRes3=modularLocalSearch(LocalSearchSettings(changeIterator(randomPermutationEncoding(n)),false),scoreFun,randomPermutationEncoding(n))
 tabuRes1=modularTabuSearch(TabuSearchSettings(100,100,100),scoreFun,randomTwoVectorEncoding(n,m))
 tabuRes2=modularTabuSearch(TabuSearchSettings(100,100,100),scoreFun,randomPermutationEncoding(n))
 tabuRes3=modularTabuSearch(TabuSearchSettings2(100,100,0.1),scoreFun,randomTwoVectorEncoding(n,m))
@@ -50,8 +51,8 @@ popul=[begin
 end for _=1:100] |> sort!
 crossw=(p1,p2)->TwoVectorEncoding(p1.machineCount,elementviseCrossover(p1.assignment,p2.assignment),pmxCrossover(p1.permutation,p2.permutation))
 geneticRes2=modularGenetic(GeneticSettings(1000,2,crossw,mutate),scoreFun,popul)
-
-sol=computeTimeWithCars(tabuRes2[2],p,k,m,c,tt)
+##
+sol=computeTimeWithCars(localRes3[2],p,k,m,c,tt)
 pl1=plotGantt(sol[1],p)
 pl2=plotCarUsage(sol[3],tt,(0,sol[2]))
-plot(pl1,pl2,layout=(2,1))
+plr=plot(pl1,pl2,layout=(2,1))
