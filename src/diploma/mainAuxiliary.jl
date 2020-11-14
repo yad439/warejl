e9f99aa676141f14037071f9e8229ed0fafe3c67
Vedblade <18503283+yad439@users.noreply.github.com>
2020-11-14 14:56:32 +0700

    Add another get only time computional function

---
+++ src/diploma/mainAuxiliary.jl
@@ -0,0 +1,52 @@
+using DataStructures
+
+include("auxiliary.jl")
+include("structures.jl")
+
+function computeTimeGetOnly(timetable::PermutationEncoding,machineCount,jobLengths,itemsNeeded,carCount,carTravelTime)
+	carNeeded=length.(itemsNeeded)
+	sums=fill(zero(eltype(jobLengths)),machineCount)
+	times=similar(jobLengths)
+	assignment=similar(jobLengths,Int)
+	inUseCars=EventQueue()
+	carHistory=Tuple{eltype(jobLengths),Int}[]
+	carsAvailable=carCount
+	availableFromTime=0
+	for job ∈ timetable.permutation
+		lastDeliverTime=0
+		itemsLeft=carNeeded[job]
+		while itemsLeft>0
+			availableAtEnd=carsAvailable
+			for event ∈ inUseCars
+				event[1]≥availableFromTime+carTravelTime && break
+				availableAtEnd+=event[2]
+			end
+			realAvailable=min(carsAvailable,availableAtEnd)
+			carsUsed=min(realAvailable,itemsLeft)
+			carsAvailable-=carsUsed
+			realAvailable-=carsUsed
+			push!(inUseCars,availableFromTime+carTravelTime,carsUsed)
+			push!(carHistory,(availableFromTime,carsUsed))
+			itemsLeft-=carsUsed
+			lastDeliverTime=availableFromTime
+			while realAvailable≤0
+				availableFromTime,carChange=pop!(inUseCars)
+				carsAvailable+=carChange
+				@assert carsAvailable≥0
+				carsAvailable==0 && continue
+				availableAtEnd=carsAvailable
+				for event ∈ inUseCars
+					event[1]≥availableFromTime+carTravelTime && break
+					availableAtEnd+=event[2]
+				end
+				realAvailable=min(carsAvailable,availableAtEnd)
+			end
+		end
+		machine=argmin(sums)
+		assignment[job]=machine
+		startTime=max(sums[machine],lastDeliverTime+carTravelTime)
+		times[job]=startTime
+		sums[machine]=startTime+jobLengths[job]
+	end
+	Schedule(assignment,times),maximum(sums),carHistory
+end
