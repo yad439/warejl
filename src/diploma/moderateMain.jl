bd5e52aefc8809ab317e7718af74d364261ce6ac
Vedblade <18503283+yad439@users.noreply.github.com>
2020-11-16 10:41:48 +0700

    Experiment

--- src/diploma/moderateMain.jl
+++ src/diploma/moderateMain.jl
@@ -9,38 +9,45 @@ using Plots
 using Printf
 using Statistics
 ##
-n=10
+n=250
 m=3
 p=rand(5:20,n)
-k=rand(1:3,n)
+k=rand(1:5,n)
 tt=10
-c=4
+c=5
 
-# scoreFun=jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt)
-scoreFun=jobs->maxTimeWithCars(jobs,p,k,m,c,tt)
+scoreFun=jobs->maxTimeWithCarsUnoptimized(jobs,p,k,m,c,tt)
+# scoreFun=jobs->maxTimeWithCars(jobs,p,k,m,c,tt)
 ##
-exactRes=moderateExact(n,m,c,p,k,tt,300)
-exactRes2=moderateExact2(n,m,c,p,k,tt,300)
-localRes1=modularLocalSearch(LocalSearchSettings(changeIterator(randomTwoVectorEncoding(n,m)),false),scoreFun,randomTwoVectorEncoding(n,m))
-localRes2=modularLocalSearch(LocalSearchSettings(randomChangeIterator(randomTwoVectorEncoding(n,m),100),false),scoreFun,randomTwoVectorEncoding(n,m))
-localRes3=modularLocalSearch(LocalSearchSettings(changeIterator(randomPermutationEncoding(n)),false),scoreFun,randomPermutationEncoding(n))
-tabuRes1=modularTabuSearch(TabuSearchSettings(100,100,100),scoreFun,randomTwoVectorEncoding(n,m))
-tabuRes2=modularTabuSearch(TabuSearchSettings(100,100,100),scoreFun,randomPermutationEncoding(n))
-tabuRes3=modularTabuSearch(TabuSearchSettings2(100,100,0.1),scoreFun,randomTwoVectorEncoding(n,m))
-tabuRes4=modularTabuSearch(TabuSearchSettings2(100,100,0.1),scoreFun,randomPermutationEncoding(n))
-tmp=randomPermutationEncoding(n)
-annealingRes1=modularAnnealing(AnnealingSettings(1000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
-tmp=randomTwoVectorEncoding(n,m)
-annealingRes2=modularAnnealing(AnnealingSettings(1000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
-tmp=randomPermutationEncoding(n)
-annealingRes3=modularAnnealing(AnnealingSettings(1000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
-tmp=randomTwoVectorEncoding(n,m)
-annealingRes4=modularAnnealing(AnnealingSettings(1000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+stsol1=rand(EncodingSample{PermutationEncoding}(n,m))
+stsol2=rand(EncodingSample{TwoVectorEncoding}(n,m))
+##
+# exactRes=moderateExact(n,m,c,p,k,tt,300)
+# exactRes2=moderateExact2(n,m,c,p,k,tt,300)
+localRes1=modularLocalSearch(LocalSearchSettings(changeIterator(stsol1),false),scoreFun,copy(stsol1))
+# localRes2=modularLocalSearch(LocalSearchSettings(randomChangeIterator(stsol1,100),false),scoreFun,copy(stsol1))
+localRes3=modularLocalSearch(LocalSearchSettings(changeIterator(stsol2),false),scoreFun,copy(stsol2))
+tabuRes1=modularTabuSearch(TabuSearchSettings(1000,1000,100),scoreFun,copy(stsol2))
+tabuRes2=modularTabuSearch(TabuSearchSettings(1000,1000,100),scoreFun,copy(stsol1))
+# tabuRes3=modularTabuSearch(TabuSearchSettings2(1000,1000,0.1),scoreFun,copy(stsol2))
+# tabuRes4=modularTabuSearch(TabuSearchSettings2(1000,1000,0.1),scoreFun,copy(stsol1))
+tmp=copy(stsol1)
+annealingRes1=modularAnnealing(AnnealingSettings(10000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
+tmp=copy(stsol2)
+annealingRes2=modularAnnealing(AnnealingSettings(10000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->new-old<threshold),scoreFun,tmp)
+tmp=copy(stsol1)
+annealingRes3=modularAnnealing(AnnealingSettings(10000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+tmp=copy(stsol2)
+annealingRes4=modularAnnealing(AnnealingSettings(10000,maxDif(tmp,scoreFun),it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+tmp=copy(stsol1)
+annealingRes5=modularAnnealing(AnnealingSettings(10000,scoreFun(tmp)*2,it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
+tmp=copy(stsol2)
+annealingRes6=modularAnnealing(AnnealingSettings(10000,scoreFun(tmp)*2,it->it*0.99,(old,new,threshold)->rand()<exp((old-new)/threshold)),scoreFun,tmp)
 popul=[begin
 	val=randomPermutationEncoding(n)
 	score=scoreFun(val)
 	GeneticEntity(val,score)
-end for _=1:100] |> sort!
+end for _=1:1000] |> sort!
 crossw=(p1,p2)->PermutationEncoding(pmxCrossover(p1.permutation,p2.permutation))
 mutate(val)=val->rand()>0.2 && randomChange!(val,it->true)
 geneticRes1=modularGenetic(GeneticSettings(1000,2,crossw,mutate),scoreFun,popul)
@@ -48,11 +55,15 @@ popul=[begin
 	val=randomTwoVectorEncoding(n,m)
 	score=scoreFun(val)
 	GeneticEntity(val,score)
-end for _=1:100] |> sort!
+end for _=1:1000] |> sort!
 crossw=(p1,p2)->TwoVectorEncoding(p1.machineCount,elementviseCrossover(p1.assignment,p2.assignment),pmxCrossover(p1.permutation,p2.permutation))
 geneticRes2=modularGenetic(GeneticSettings(1000,2,crossw,mutate),scoreFun,popul)
 ##
-sol=computeTimeWithCars(localRes3[2],p,k,m,c,tt)
-pl1=plotGantt(sol[1],p)
+ress=[localRes1,localRes3,tabuRes1,tabuRes2,annealingRes1,annealingRes2,annealingRes3,annealingRes4,annealingRes5,annealingRes6,geneticRes1,geneticRes2]
+rrs=["local 1","local 3","tabu 1","tabu 2","anealing 1","anealing 2","anealing 3","anealing 4","anealing 5","anealing 6","genetic 1","genetic 2"]
+foreach((a,b)->println(b,": ",a[1]),ress,rrs)
+##
+sol=computeTimeWithCars(tabuRes1[2],p,k,m,c,tt)
+pl1=plotGantt(sol[1],p,false)
 pl2=plotCarUsage(sol[3],tt,(0,sol[2]))
 plr=plot(pl1,pl2,layout=(2,1))
