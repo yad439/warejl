81a4a71c7e730ff37c8db082662cc0b18605ce2a
Vedblade <18503283+yad439@users.noreply.github.com>
2020-11-20 19:38:45 +0700

    Add compute time cancel return function

--- src/diploma/mainAuxiliary.jl
+++ src/diploma/mainAuxiliary.jl
@@ -52,7 +52,6 @@ function computeTimeGetOnly(timetable::PermutationEncoding,machineCount,jobLengt
 end
 
 function computeTimeGetOnlyWaitOne(timetable::PermutationEncoding,machineCount,jobLengths,itemsNeeded,carCount,carTravelTime)
-	carNeeded=length.(itemsNeeded)
 	sums=fill(zero(eltype(jobLengths)),machineCount)
 	times=similar(jobLengths)
 	assignment=similar(jobLengths,Int)
@@ -120,14 +119,8 @@ function computeTimeNoWait(timetable::PermutationEncoding,machineCount,jobLength
 				availableAtEnd+=event[2]
 			end
 			realAvailable=min(carsAvailable,availableAtEnd)
-			carsUsed=min(realAvailable,itemsLeft)
-			carsAvailable-=carsUsed
-			realAvailable-=carsUsed
-			push!(inUseCars,availableFromTime+carTravelTime,carsUsed)
-			push!(carHistory,(availableFromTime,(job,carsUsed,true)))
-			itemsLeft-=carsUsed
-			lastDeliverTime=availableFromTime
 			while realAvailable≤0
+				@assert realAvailable==0
 				availableFromTime,carChange=pop!(inUseCars)
 				carsAvailable+=carChange
 				@assert carsAvailable≥0
@@ -139,6 +132,12 @@ function computeTimeNoWait(timetable::PermutationEncoding,machineCount,jobLength
 				end
 				realAvailable=min(carsAvailable,availableAtEnd)
 			end
+			carsUsed=min(realAvailable,itemsLeft)
+			carsAvailable-=carsUsed
+			push!(inUseCars,availableFromTime+carTravelTime,carsUsed)
+			push!(carHistory,(availableFromTime,(job,carsUsed,true)))
+			itemsLeft-=carsUsed
+			lastDeliverTime=availableFromTime
 		end
 		machine=argmin(sums)
 		assignment[job]=machine
@@ -181,6 +180,105 @@ function computeTimeNoWait(timetable::PermutationEncoding,machineCount,jobLength
 				end
 				realAvailable=min(backAvailable,availableAtEnd)
 			end
+		end
+	end
+	Schedule(assignment,times),maximum(sums),carHistory
+end
+
+function computeTimeCancelReturn(timetable::PermutationEncoding,machineCount,jobLengths,itemsNeeded,carCount,carTravelTime)
+	sums=fill(zero(eltype(jobLengths)),machineCount)
+	times=similar(jobLengths)
+	assignment=similar(jobLengths,Int)
+	inUseCars=EventQueue2()
+	carHistory=Tuple{eltype(jobLengths),Tuple{Int,Int,Bool}}[]
+	carsAvailable=carCount
+	availableFromTime=0
+	for job ∈ timetable.permutation
+		lastDeliverTime=0
+		itemsLeft=itemsNeeded[job]
+		while length(itemsLeft)>0
+			availableAtEnd=carsAvailable
+			for event ∈ inUseCars
+				event[1][1]≥availableFromTime+carTravelTime && break
+				if event[1][2]
+					inter=intersect(itemsLeft,event[2].remove)
+					setdiff!(itemsLeft,inter)
+					setdiff!(event[2].remove)
+				end
+				availableAtEnd-=length(event[2])*(2Int(event[1][2])-1)
+			end
+			realAvailable=min(carsAvailable,availableAtEnd)
+			while realAvailable≤0
+				@assert realAvailable==0
+				(availableFromTime,isNew),carChange=pop!(inUseCars)
+				carsAvailable-=length(carChange)*(2Int(isNew)-1)
+				@assert carsAvailable≥0
+				carsAvailable==0 && continue
+				availableAtEnd=carsAvailable
+				for event ∈ inUseCars
+					event[1][1]≥availableFromTime+carTravelTime && break
+					if event[1][2]
+						inter=intersect(itemsLeft,event[2].remove)
+						setdiff!(itemsLeft,inter)
+						setdiff!(event[2].remove)
+					end
+					availableAtEnd-=length(event[2])*(2Int(event[1][2])-1)
+				end
+				realAvailable=min(carsAvailable,availableAtEnd)
+			end
+			carsUsed=min(realAvailable,itemsLeft)
+			carsAvailable-=carsUsed
+			items=Iterators.take(itemsLeft,carsUsed)
+			for item ∈ items
+				push!(inUseCars,availableFromTime+carTravelTime,false,true,item)
+			end
+			push!(carHistory,(availableFromTime,(job,carsUsed,true)))
+			setdiff!(itemsLeft,items)
+			lastDeliverTime=availableFromTime
+		end
+		machine=argmin(sums)
+		assignment[job]=machine
+		startTime=max(sums[machine],lastDeliverTime+carTravelTime)
+		times[job]=startTime
+		sums[machine]=startTime+jobLengths[job]
+
+		backAvailableFrom=startTime+jobLengths[job]
+		itemsLeft=itemsNeeded[job]
+		backAvailable=carsAvailable
+		inUseCars2=copy(inUseCars)
+		while !isempty(inUseCars2) && first(inUseCars2)[1][1]<backAvailableFrom
+			event=pop!(inUseCars2)
+			backAvailable-=length(event[2])*(2Int(event[1][2])-1)
+		end
+		while length(itemsLeft)>0
+			availableAtEnd=backAvailable
+			for event ∈ inUseCars
+				event[1][1]≥availableFromTime+carTravelTime && break
+				availableAtEnd-=length(event[2])*(2Int(event[1][2])-1)
+			end
+			realAvailable=min(backAvailable,availableAtEnd)
+			while realAvailable==0
+				(backAvailableFrom,isNew),carChange=pop!(inUseCars2)
+				backAvailable-=length(carChange)*(2Int(isNew)-1)
+				@assert backAvailable≥0
+				backAvailable==0 && continue
+				availableAtEnd=backAvailable
+				for event ∈ inUseCars2
+					event[1]≥backAvailable+carTravelTime && break
+					availableAtEnd-=length(event[2])*(2Int(event[1][2])-1)
+				end
+				@assert availableAtEnd≥0
+				realAvailable=min(backAvailable,availableAtEnd)
+			end
+			carsUsed=min(realAvailable,itemsLeft)
+			backAvailable-=carsUsed
+			items=Iterators.take(itemsLeft,carsUsed)
+			entry=EventEntry(BitSet(),BitSet(items))
+			push!(inUseCars,backAvailableFrom,true,entry)
+			push!(inUseCars,backAvailableFrom+carTravelTime,false,entry)
+			push!(inUseCars2,backAvailableFrom+carTravelTime,false,entry)
+			push!(carHistory,(backAvailableFrom,(job,carsUsed,false)))
+			setdiff!(itemsLeft,items)
 		end
 	end
 	Schedule(assignment,times),maximum(sums),carHistory
