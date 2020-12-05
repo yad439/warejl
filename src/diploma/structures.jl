cd64b1ec26913e2a20b08eeb2496540c04dbefa0
Vedblade <18503283+yad439@users.noreply.github.com>
2020-12-05 17:59:15 +0700

    Add full event queue

--- src/diploma/structures.jl
+++ src/diploma/structures.jl
@@ -87,3 +87,66 @@ first(queue::EventQueue2)=first(queue.data)
 isempty(queue::EventQueue2)=isempty(queue.data)
 length(entry::EventEntry)=length(entry.add)+length(entry.remove)
 copy(entry::EventEntry)=EventEntry(copy(entry.add),copy(entry.remove))
+
+struct EventEntry3
+	startAdd::Nothing
+	startRemove::BitSet
+	endAdd::BitSet
+	endRemove::BitSet
+end
+EventEntry3()=EventEntry3(nothing,BitSet(),BitSet(),BitSet())
+struct EventQueue3
+	data::SortedDict{Int,EventEntry3}
+end
+EventQueue3()=EventQueue3(SortedDict{Int,EventEntry3}())
+eltype(::Type{EventQueue3})=Pair{Int,EventEntry3}
+length(queue::EventQueue3)=length(queue.data)
+iterate(queue::EventQueue3)=iterate(queue.data)
+iterate(queue::EventQueue3,state)=iterate(queue.data,state)
+function push!(queue::EventQueue3,time,new,add::Bool,item::Int)
+	entry=get!(queue.data,time,EventEntry3())
+	if add
+		if new
+			@assert false
+			@assert entry.startAdd ∌ item
+			push!(entry.startAdd,item)
+		else
+			@assert entry.endAdd ∌ item
+			push!(entry.endAdd,item)
+		end
+	else
+		if new
+			@assert entry.startRemove ∌ item
+			push!(entry.startRemove,item)
+		else
+			@assert entry.endRemove ∌ item
+			push!(entry.endRemove,item)
+		end
+	end
+end
+function append!(queue::EventQueue3,time,new,add::Bool,items)
+	entry=get!(queue.data,time,EventEntry3())
+	if add
+		if new
+			@assert false
+			@assert isdisjoint(entry.startAdd,items)
+			union!(entry.startAdd,items)
+		else
+			@assert isdisjoint(entry.endAdd,items)
+			union!(entry.endAdd,items)
+		end
+	else
+		if new
+			@assert isdisjoint(entry.startRemove,items)
+			union!(entry.startRemove,items)
+		else
+			@assert isdisjoint(entry.endRemove,items)
+			union!(entry.endRemove,items)
+		end
+	end
+end
+function popfirst!(queue::EventQueue3)
+	ret=first(queue.data)
+	pop!(queue.data,ret[1])
+	ret
+end
