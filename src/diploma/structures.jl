eaec41037f2388af276db62c62c7b9dcced37d39
Vedblade <18503283+yad439@users.noreply.github.com>
2020-12-05 13:20:06 +0700

    Refactor: remove redundunt comments

--- src/diploma/structures.jl
+++ src/diploma/structures.jl
@@ -51,7 +51,7 @@ end
 function push!(queue::EventQueue2,time,new,entry::EventEntry,dup::Bool=false)
 	if haskey(queue.data,(time,new))
 		if dup
-			@assert queue.data[(time,new)]≡entry #todo enable
+			@assert queue.data[(time,new)]≡entry
 		else
 			@assert isdisjoint(queue.data[(time,new)].add,entry.add)
 			@assert isdisjoint(queue.data[(time,new)].remove,entry.remove)
