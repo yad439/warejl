4e9ea07abedd4caf5a1fa37e870ddb3308305c74
Vedblade <18503283+yad439@users.noreply.github.com>
2020-12-06 13:40:53 +0700

    Add compute time lazy return function

--- src/diploma/mainAuxiliary.jl
+++ src/diploma/mainAuxiliary.jl
@@ -416,3 +416,80 @@ function computeTimeCancelReturn(timetable,machineCount,jobLengths,itemsNeeded,c
 	foreach(event->push!(carHistory,(event[1],event[2])),inUseCars)
 	Schedule(assignment,times),maximum(sums),carHistory,bigHistory
 end
+
+function computeTimeLazyReturn(timetable,machineCount,jobLengths,itemsNeeded,carCount,carTravelTime,bufferSize)
+	sums=fill(zero(eltype(jobLengths)),machineCount)
+	times=similar(jobLengths)
+	assignment=similar(jobLengths,Int)
+	inUseCars=EventQueue3()
+	carHistory=Tuple{Int,EventEntry3}[]
+	bigHistory=Vector{Pair{Int,EventEntry3}}[]
+	carsAvailable=carCount
+	availableFromTime=0 # points at last add travel start
+	bufferState=BitSet()
+	lockTime=Dict{Int,Int}()
+	for job ∈ timetable.permutation
+		lastDeliverTime=0
+		itemsLeft=setdiff(itemsNeeded[job],bufferState)
+		while length(itemsLeft)>0
+			while carsAvailable≤0
+				@assert carsAvailable≥0
+				(availableFromTime,carChange)=popfirst!(inUseCars)
+				carsAvailable+=length(carChange.endAdd)+length(carChange.endRemove)-length(carChange.startRemove)
+				push!(carHistory,(availableFromTime,carChange))
+				# setdiff!(bufferState,carChange.startRemove)
+				# union!(bufferState,carChange.endAdd)
+				@assert carsAvailable≥0
+			end
+			if length(bufferState)<bufferSize
+				carsUsed=min(carsAvailable,bufferSize-length(bufferState),length(itemsLeft))
+				toAdd=Iterators.take(itemsLeft,carsUsed)
+				carsAvailable-=carsUsed
+				append!(inUseCars,availableFromTime+carTravelTime,false,true,toAdd)
+				@assert isdisjoint(bufferState,toAdd)
+				@assert toAdd ⊆ itemsLeft
+				union!(bufferState,toAdd)
+				setdiff!(itemsLeft,toAdd)
+			else
+				activeLocks=Iterators.map(it->(it,lockTime[it]),Iterators.filter(it->it∉itemsNeeded[job],bufferState))
+				minLockTime=minimum(lock[2] for lock ∈ activeLocks)
+				while !isempty(inUseCars) && first(inUseCars)[1]≤minLockTime-carTravelTime
+					(availableFromTime,carChange)=popfirst!(inUseCars)
+					carsAvailable+=length(carChange.endAdd)+length(carChange.endRemove)-length(carChange.startRemove)
+					push!(carHistory,(availableFromTime,carChange))
+				end
+				availableFromTime=max(availableFromTime,minLockTime-carTravelTime)
+				minLocks=Iterators.map(first,Iterators.filter(it->it[2]==minLockTime,activeLocks))
+				changesNum=min(carsAvailable,length(minLocks),length(itemsLeft))
+				toRemove=Iterators.take(minLocks,changesNum)
+				toAdd=Iterators.take(itemsLeft,changesNum)
+				for item ∈ toRemove
+					delete!(lockTime,item)
+				end
+				carsAvailable-=changesNum
+				append!(inUseCars,availableFromTime+carTravelTime,false,true,toAdd)
+				append!(inUseCars,availableFromTime+carTravelTime,true,false,toRemove)
+				append!(inUseCars,availableFromTime+2carTravelTime,false,false,toRemove)
+				@assert toRemove ⊆ bufferState
+				@assert isdisjoint(bufferState,toAdd)
+				@assert toAdd ⊆ itemsLeft
+				setdiff!(bufferState,toRemove)
+				union!(bufferState,toAdd)
+				setdiff!(itemsLeft,toAdd)
+			end
+			@assert length(bufferState)≤bufferSize
+			@assert carsAvailable≥0
+		end
+		machine=selectMachine(job,timetable,sums)
+		assignment[job]=machine
+		startTime=max(sums[machine],lastDeliverTime+carTravelTime)
+		times[job]=startTime
+		sums[machine]=startTime+jobLengths[job]
+		for item ∈ itemsNeeded[job]
+			lockTime[item]=max(get(lockTime,item,0),startTime+jobLengths[job])
+		end
+		push!(bigHistory,deepcopy(inUseCars|>collect))
+	end
+	foreach(event->push!(carHistory,(event[1],event[2])),inUseCars)
+	Schedule(assignment,times),maximum(sums),carHistory,bigHistory
+end
