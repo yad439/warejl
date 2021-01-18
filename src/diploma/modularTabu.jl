c73a5ed33acd536e4ff3c2e53d22cc4dc1e01bd7
Vedblade <18503283+yad439@users.noreply.github.com>
2021-01-18 21:39:35 +0700

    Add temporary strict tabu search

--- src/diploma/modularTabu.jl
+++ src/diploma/modularTabu.jl
@@ -49,6 +49,44 @@ function modularTabuSearch(settings,scoreFunction,startTimeTable)
 	(score=minval,solution=minsol,history=history)
 end
 
+function modularTabuSearch2(settings,scoreFunction,startTimeTable)
+	progress=ProgressUnknown("Local tabu search:")
+
+	timeTable=startTimeTable
+	tabu=Queue{Int}()
+	minval=scoreFunction(timeTable)
+	minsol=copy(timeTable)
+	counter=0
+
+	history=QHistory(typeof(minval))
+	push!(history,minval)
+	while counter<settings.searchTries
+		newTimeTableChange=modularTabuImprove2(timeTable,tabu,settings.neighbourhoodSize,scoreFunction)
+		restoreChange=change!(timeTable,newTimeTableChange)
+		if newTimeTableChange[1]≡PERMUTATION_MOVE
+			enqueue!(tabu,newTimeTableChange[2])
+		elseif newTimeTableChange[1]≡PERMUTATION_SWAP
+			enqueue!(tabu,newTimeTableChange[2])
+			enqueue!(tabu,newTimeTableChange[3])
+		end
+		score=scoreFunction(timeTable)
+		push!(history,score)
+		if score<minval
+			counter=0
+			minval=score
+			copy!(minsol,timeTable)
+		else
+			counter+=1
+		end
+		while length(tabu)>settings.tabuSize
+			dequeue!(tabu)
+		end
+		ProgressMeter.next!(progress,showvalues=(("Score",score),))
+	end
+	ProgressMeter.finish!(progress)
+	(score=minval,solution=minsol,history=history)
+end
+
 function modularTabuImprove(timeTable,tabu,neighbourhoodSize::Int,scoreFunction)
 	minval=typemax(Int)
 	toApply=(defaultChange(timeTable),0,0)
@@ -81,6 +119,38 @@ function modularTabuImprove(timetable,tabu,neighbourhoodProbability::Float64,sco
 	toApply
 end
 
+function modularTabuImprove2(timeTable,tabu,neighbourhoodSize::Int,scoreFunction)
+	minval=typemax(Int)
+	toApply=(defaultChange(timeTable),0,0)
+	for _=1:neighbourhoodSize
+		newChange,restoreChange=randomChange!(timeTable,change->tabuCanChange2(timeTable,change,tabu))
+		score=scoreFunction(timeTable)
+		change!(timeTable,restoreChange)
+		if score<minval
+			minval=score
+			toApply=newChange
+		end
+	end
+	toApply
+end
+
+function modularTabuImprove2(timetable,tabu,neighbourhoodProbability::Float64,scoreFunction)
+	minval=typemax(Int)
+	toApply=(0,0,0)
+	for change ∈ changeIterator(timetable)
+		rand() > neighbourhoodProbability && continue
+		tabuCanChange2(timetable,change,tabu) || continue
+		restoreChange=change!(timetable,change)
+		score=scoreFunction(timetable)
+		change!(timetable,restoreChange)
+		if score<minval
+			minval=score
+			toApply=change
+		end
+	end
+	toApply
+end
+
 function tabuCanChange(::TwoVectorEncoding,change,tabu)
 	if change[1]==TWO_VECTOR_MOVE_ORDER || change[1]==TWO_VECTOR_MOVE_ASSIGNMENT
 		return change ∉ tabu
@@ -95,6 +165,15 @@ function tabuCanChange(::PermutationEncoding,change,tabu)
 		return change ∉ tabu
 	elseif change[1]==PERMUTATION_SWAP
 		return (PERMUTATION_SWAP,change[2],change[3]) ∉ tabu && (PERMUTATION_SWAP,change[3],change[2]) ∉ tabu
+	end
+	@assert(false)
+end
+
+function tabuCanChange2(::PermutationEncoding,change,tabu)
+	if change[1]==PERMUTATION_MOVE
+		return change[2] ∉ tabu
+	elseif change[1]==PERMUTATION_SWAP
+		return change[2] ∉ tabu && change[3] ∉ tabu
 	end
 	@assert(false)
 end
