53f015fc7bc4f7d2639855b8eb429dcc598667a4
Vedblade <18503283+yad439@users.noreply.github.com>
2021-01-28 15:35:56 +0700

    Improve score function tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,19 +5,21 @@ include("../src/utility.jl")
 include("../src/mainAuxiliary.jl")
 
 Random.seed!(1234)
-
-@testset "Lazy return time computing of $enc" for enc in [PermutationEncoding,TwoVectorEncoding]
-	for n=1:40,m=1:10,c=1:10
-		samp=EncodingSample{enc}(n,m)
-		p=rand(5:20,n)
-		itemCount=14
-		itemsNeeded=[randchoice(1:itemCount,rand(1:8)) for _=1:n]
-		itemsNeeded=map(BitSet,itemsNeeded)
-		tt=20
-		k=length.(itemsNeeded)
-		bs=maximum(length.(itemsNeeded))+rand(0:4)
-		s=rand(samp)
-		sol=@inferred computeTimeLazyReturn(s,m,p,itemsNeeded,c,tt,bs,Val(true))
-		@test sol.time==@inferred computeTimeLazyReturn(s,m,p,itemsNeeded,c,tt,bs,Val(false))
+@testset "Score function tests" begin
+	@testset "Lazy return time computing of $enc" for enc in [PermutationEncoding,TwoVectorEncoding]
+		for n=[5,10,20,40],m=[1,2,3,5,10],c=[1,2,5,10,20]
+			samp=EncodingSample{enc}(n,m)
+			p=rand(5:20,n)
+			itemCount=14
+			itemsNeeded=[randchoice(1:itemCount,rand(1:8)) for _=1:n]
+			itemsNeeded=map(BitSet,itemsNeeded)
+			tt=20
+			k=length.(itemsNeeded)
+			bs=maximum(length.(itemsNeeded))+rand(0:4)
+			problem=Problem(n,m,c,tt,itemCount,bs,p,itemsNeeded)
+			s=rand(samp)
+			sol=@inferred computeTimeLazyReturn(s,problem,Val(true))
+			@test sol.time==@inferred computeTimeLazyReturn(s,problem,Val(false))
+		end
 	end
 end
