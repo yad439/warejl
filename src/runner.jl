353bba3dc5b144f2588fdcfd7d594124702f6c1d
Vedblade <18503283+yad439@users.noreply.github.com>
2021-02-05 12:52:24 +0700

    Experiment optimal tabu size

--- src/runner.jl
+++ src/runner.jl
@@ -17,10 +17,11 @@ using CSV
 using Statistics
 #using ProgressMeter
 
-machineCount=4
+machineCount=6
 carCount=30
 bufferSize=6
 problem=Problem(parseRealData("res/benchmark - automatic warehouse",20,4),machineCount,carCount,bufferSize,box->box.lineType=="A")
+@assert isValid(problem)
 sf=let problem=problem
 	jobs->computeTimeLazyReturn(jobs,problem,Val(false))
 end
@@ -33,14 +34,20 @@ st1=rand(sample1)
 
 #exactModel=buildModel(problem,ORDER_FIRST,SEPARATE_EVENTS,T)
 #exactRes=runModel(exactModel,1800)
-#df=DataFrame(problemSize=Int[],problemNum=Int[],bs=Int[],time=Int[],cars=Int[])
-starts=rand(sample1,5)
-res=map([50,100,200,400,800]) do tabuSize
-	println("Size: ",tabuSize)
-	tabuSettings=TabuSearchSettings(1000,tabuSize,1000)
-	ress=ThreadsX.map(1:5,basesize=2) do i
-		modularTabuSearch3(tabuSettings,sf,deepcopy(starts[i]),false).score
+df=CSV.File("test/tabuRes.tsv") |> DataFrame
+starts=rand(sample1,10)
+sizes=[10,25,50,100,200,400,800]
+prog=Progress(10*length(sizes))
+res=map(sizes) do tabuSize
+	#println("Size: ",tabuSize)
+	tabuSettings=TabuSearchSettings(1000,tabuSize,2*problem.jobCount^2)
+	ress=ThreadsX.map(1:10) do i
+		sc=modularTabuSearch5(tabuSettings,sf,deepcopy(starts[i]),false).score
+		ProgressMeter.next!(prog)
+		sc
 	end
+	push!(df,(20,4,"A",missing,problem.jobCount,machineCount,carCount,bufferSize,5,1000,tabuSize,2*problem.jobCount^2,minimum(ress),maximum(ress),mean(ress)))
 	minimum(ress),maximum(ress),mean(ress)
 end
+ProgressMeter.finish!(prog);
 #println(minimum(res),' ',maximum(res),' ',mean(res))
