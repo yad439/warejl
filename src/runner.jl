274de613739eebcf44b329ef9ec1330e13cc1e66
Vedblade <18503283+yad439@users.noreply.github.com>
2021-02-16 19:51:51 +0700

    Experiment big neightbourhood size

--- src/runner.jl
+++ src/runner.jl
@@ -17,17 +17,18 @@ using CSV
 using Statistics
 #using ProgressMeter
 
-machineCount=6
-carCount=30
-bufferSize=6
-problem=Problem(parseRealData("res/benchmark - automatic warehouse",20,4),machineCount,carCount,bufferSize,box->box.lineType=="A")
+machineCount=8
+carCount=20
+bufferSize=5
+problem=Problem(parseRealData("res/benchmark - automatic warehouse",100,1),machineCount,carCount,bufferSize,box->box.lineType=="A")
 @assert isValid(problem)
+@assert problem.bufferSize>=maximum(length,problem.itemsNeeded)
 sf=let problem=problem
 	jobs->computeTimeLazyReturn(jobs,problem,Val(false))
 end
 sample1=EncodingSample{PermutationEncoding}(problem.jobCount,problem.machineCount)
 sample2=EncodingSample{TwoVectorEncoding}(problem.jobCount,problem.machineCount);
-
+println(problem.jobCount)
 #st1=rand(sample1)
 #sol=computeTimeLazyReturn(st1,problem,Val(true))
 #T=sol.schedule.carTasks |> ffilter(e->e.isAdd) |> fmap(e->e.time) |> unique |> length
@@ -35,22 +36,23 @@ sample2=EncodingSample{TwoVectorEncoding}(problem.jobCount,problem.machineCount)
 #M=sol.time
 #println(M,' ',T)
 
-#exactModel=buildModel(problem,ORDER_FIRST_STRICT,SHARED_EVENTS_QUAD,T,M)
+#exactModel=buildModel(problem,ORDER_FIRST_STRICT,SHARED_EVENTS,T,M)
 #exactRes=runModel(exactModel,30*60)
 df=CSV.File("test/tabuRes.tsv") |> DataFrame
 starts=rand(sample1,10)
-sizes=[10,100,500,1000,1400]
+sizes=[50,100,500,1000,2000]
 prog=Progress(10*length(sizes))
-res=map(sizes) do neiSize
-	#println("Size: ",tabuSize)
-	tabuSettings=TabuSearchSettings(1500,100,neiSize)
+res=map(sizes) do tabuSize
+	println("Size: ",tabuSize)
+	tabuSettings=TabuSearchSettings(2000,tabuSize,1000)
 	ress=ThreadsX.map(1:10) do i
 		sc=modularTabuSearch5(tabuSettings,sf,deepcopy(starts[i]),false).score
 		ProgressMeter.next!(prog)
+		println(i)
 		sc
 	end
-	push!(df,(20,4,"A",missing,problem.jobCount,machineCount,carCount,bufferSize,5,1500,100,neiSize,minimum(ress),maximum(ress),mean(ress)))
-	neiSize,minimum(ress),maximum(ress),mean(ress)
+	push!(df,(100,1,"A",missing,problem.jobCount,machineCount,carCount,bufferSize,5,2000,tabuSize,1000,minimum(ress),maximum(ress),mean(ress)))
+	tabuSize,minimum(ress),maximum(ress),mean(ress)
 end
 ProgressMeter.finish!(prog);
 #println(minimum(res),' ',maximum(res),' ',mean(res))
