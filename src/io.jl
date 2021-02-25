968c004dab4f5ba9023e35242d8ca0d497bed4d6
Vedblade <18503283+yad439@users.noreply.github.com>
2021-02-25 22:35:15 +0700

    Fix items numeration in json output

--- src/io.jl
+++ src/io.jl
@@ -4,6 +4,9 @@ toSnakeCase(str)=mapreduce(c->isuppercase(c) ? "_$(lowercase(c))" : "$c",*,colle
 
 function toJson(file,problem)
 	d=Dict(map(field->toSnakeCase(string(field))=>getfield(problem,field),fieldnames(typeof(problem))))
+	d["items_needed"]=map(d["items_needed"]) do items
+		map(x->x-1,collect(items))
+	end
 	write(file,JSON.json(d))
 	nothing
 end
