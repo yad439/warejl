db0e87b176cbf2de9be4c8c4365ef9fdfef498af
Vedblade <18503283+yad439@users.noreply.github.com>
2021-05-29 14:22:32 +0700

    Experiment: prepare for big computations

--- src/runner.jl
+++ src/runner.jl
@@ -150,18 +150,19 @@ res=map(temps) do temp
 end
 CSV.write("exp/annRes.tsv",df,delim='\t')
 =#
-#=
+
 df=CSV.File("exp/tabuRes.tsv") |> DataFrame
-starts=rand(sample1,10)
-#st4=PermutationEncoding(likehoodBased(jobDistance(getfield(problem,:itemsNeeded)),argmin([sf(PermutationEncoding(likehoodBased(jobDistance(getfield(problem,:itemsNeeded)),i))) for i=1:getfield(problem,:jobCount)])));
-#starts=fill(st4,10)
-tabuSize=300
-baseIter=1500
-neighSize=round(Int,1500*2.077168272078332/1.3165544511496166)
+#starts=rand(sample1,10)
+st4=PermutationEncoding(likehoodBased(jobDistance(getfield(problem,:itemsNeeded)),argmin([sf(PermutationEncoding(likehoodBased(jobDistance(getfield(problem,:itemsNeeded)),i))) for i=1:getfield(problem,:jobCount)])));
+starts=fill(st4,10)
+tabuSize=1038
+baseIter=3000
+#neighSize=round(Int,1500*2.077168272078332/1.3165544511496166)
+neighSize=5000
 tabuSettings=TabuSearchSettings(baseIter,tabuSize,neighSize)
 #rdm=PermutationRandomIterable(problem.jobCount,neighSize,0.5,jobDistance(problem.itemsNeeded))
 #tabuSettings=TabuSearchSettings4(baseIter,tabuSize,rdm)
-ress2=progress_map(mapfun=tmap,1:10) do i
+ress2=progress_map(mapfun=ThreadsX.map,1:10) do i
 	#println("Start $i")
 	sc=modularTabuSearch5(tabuSettings,sf2,deepcopy(starts[i]),i==1)
 	#ProgressMeter.next!(prog)
@@ -170,9 +171,9 @@ ress2=progress_map(mapfun=tmap,1:10) do i
 end
 ress=map(first,ress2)
 iters=map(secondElement,ress2)
-push!(df,(probSize,probNum,"A",missing,problem.jobCount,machineCount,carCount,bufferSize,false,5,tabuSettings.searchTries,tabuSettings.tabuSize,neighSize,0.5,"none",minimum(ress),maximum(ress),mean(ress),minimum(iters),maximum(iters),mean(iters)))
+push!(df,(probSize,probNum,"A",missing,problem.jobCount,machineCount,carCount,bufferSize,false,5,tabuSettings.searchTries,tabuSettings.tabuSize,neighSize,0.5,"bestStart",minimum(ress),maximum(ress),mean(ress),minimum(iters),maximum(iters),mean(iters)))
 CSV.write("exp/tabuRes.tsv",df,delim='\t')
-=#
+
 #=
 prob=Problem(9,3,2,2,8,3,[10,2,8,5,6,6,4,2,1],BitSet.([[1],[2],[2],[3],[4],[5],[6],[6,7],[6,7,8]]))
 @assert isValid(prob)
@@ -213,32 +214,37 @@ CSV.write("exp/tabuRes.tsv",df,delim='\t')
 =#
 #=
 df=CSV.File("exp/annRes.tsv") |> DataFrame
+dist=jobDistance(problem.itemsNeeded)
 starts=rand(sample1,10)
 #st4=PermutationEncoding(likehoodBased(jobDistance(getfield(problem,:itemsNeeded)),argmin([sf(PermutationEncoding(likehoodBased(jobDistance(getfield(problem,:itemsNeeded)),i))) for i=1:getfield(problem,:jobCount)])));
 #starts=fill(st4,10)
 #power=1-10^-4
 # prog=Progress(10*length(pows))
 dif=maxDif(starts[1],sf2)
-temp=dif
+temp=dif/2
 dyn=false
-same=1
-#steps=10^7
-steps=round(Int,10^6*7.2321310304/3.4153597199)
+same=100_000
+steps=2*10^7
+#steps=round(Int,10^6*7.2321310304/3.4153597199)
 #steps=round(Int,-log(power,-2dif*log(10^-3)))
 #steps=round(Int,stepsBase/)
 power=(-temp*log(10^-3))^(-1/(steps/same))
-annealingSettings=AnnealingSettings(steps,dyn,same,temp,it->it*power,(old,new,threshold)->rand()<exp((old-new)/threshold))
-ress=tmap(1:10) do i
+# annealingSettings=AnnealingSettings(steps,dyn,same,temp,it->it*power,(old,new,threshold)->rand()<exp((old-new)/threshold))
+rdm=let dist=dist
+	jobs->controlledPermutationRandom(jobs,0.5,dist)
+end
+annealingSettings=AnnealingSettings2(steps,false,same,dif/2,it->it*power,(old,new,threshold)->rand()<exp((old-new)/threshold),rdm)
+ress=ThreadX.map(1:10) do i
 	println("Start $i")
-	sc=modularAnnealing(annealingSettings,sf2,deepcopy(starts[i]),false).score
+	sc=modularAnnealing(annealingSettings,sf2,deepcopy(starts[i]),false)
 	#ProgressMeter.next!(prog)
 	println("End $i")
-	sc
+	sf(sc.solution)
 end
-push!(df,(probSize,probNum,"A",missing,problem.jobCount,machineCount,carCount,bufferSize,false,annealingSettings.searchTries,dyn,annealingSettings.sameTemperatureTries,dif,annealingSettings.startTheshold,power,0.5,"none",minimum(ress),maximum(ress),mean(ress)))
+push!(df,(probSize,probNum,"A",missing,problem.jobCount,machineCount,carCount,bufferSize,false,annealingSettings.searchTries,dyn,annealingSettings.sameTemperatureTries,dif/2,annealingSettings.startTheshold,power,0.5,"itemBased",minimum(ress),maximum(ress),mean(ress)))
 CSV.write("exp/annRes.tsv",df,delim='\t')
 =#
-
+#=
 sts=rand(sample1,10^3)
 tabuSettings=TabuSearchSettings(100,100,100)
 annealingSettings=AnnealingSettings(10^3,false,1,1000,it->it*0.999,(old,new,threshold)->rand()<exp((old-new)/threshold))
@@ -273,7 +279,7 @@ println("$time1 $time4 $time5")
 df=CSV.File("exp/times.tsv")|>DataFrame
 push!(df,(problem.jobCount,gethostname(),time0,time2,time3,time1,time4,time5))
 CSV.write("exp/times.tsv",df,delim='\t')
-
+=#
 #=
 res=progress_map(1:10^4,mapfun=ThreadsX.map) do _
 	st=rand(sample1)
