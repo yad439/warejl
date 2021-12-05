abefa7692fea77f181582ee6824ab0773eaccd10
Vedblade <18503283+yad439@users.noreply.github.com>
2021-12-05 17:55:22 +0700

    Experiment hybrid tabu

--- src/runner.jl
+++ src/runner.jl
@@ -1,6 +1,7 @@
 include("mainAuxiliary.jl");
 include("tabu.jl");
 include("annealing.jl");
+include("hybridTabu.jl");
 include("realDataUtility.jl");
 include("linear.jl");
 include("extendedRandoms.jl");
@@ -13,7 +14,7 @@ using Random
 import JSON
 
 ##
-#=
+
 let
 	resFile = "exp/results.json"
 
@@ -25,79 +26,89 @@ let
 
 	results = fromJson(Vector{ProblemInstance}, JSON.parsefile(resFile))
 	try
-		for probNum = [4] # [1, 4, 8] [2, 7, 10]
-		# for _ = [0]
-			println("Instance ", probNum)
-			let
-				# bufferSize = problemStats(probSize, probNum, ['A']).maxItems
+		# for probNum = [4] # [1, 4, 8] [2, 7, 10]
+		for _ in [0]
+		    # println("Instance ", probNum)
+		    let
+		        # bufferSize = problemStats(probSize, probNum, ['A']).maxItems
 
-				instance = findInstance(
-									results,probSize,probNum,['A'],
-									missing,machineCount,carCount,bufferSize
-							)
-				if instance ≡ nothing
-					instance = createInstance(
-									probSize,probNum,['A'],
-									missing,machineCount,carCount,bufferSize
-							)
-					push!(results, instance)
-				end
-				instance::ProblemInstance
+		        # instance = findInstance(
+		        # 					results,probSize,probNum,['A'],
+		        # 					missing,machineCount,carCount,bufferSize
+		        # 			)
+		        instance = results[47]
+		        @assert instance.problemSize == 50
+		        @assert instance.problemNumber == 2
+		        probNum = 2
+		        # if instance ≡ nothing
+		        # 	instance = createInstance(
+		        # 					probSize,probNum,['A'],
+		        # 					missing,machineCount,carCount,bufferSize
+		        # 			)
+		        # 	push!(results, instance)
+		        # end
+		        instance::ProblemInstance
 
-				problem = try
-					instanceToProblem(instance)
-				catch e
-					println(stderr, "Can't parse problem ", probNum)
-					continue
-				end
-				problem::Problem
-				if !isValid(problem)
-					println(stderr, "Problem ", probNum, " is invalid!")
-					continue
-				end
+		        problem = try
+		            instanceToProblem(instance)
+		        catch e
+		            println(stderr, "Can't parse problem ", probNum)
+		            continue
+		        end
+		        problem::Problem
+		        if !isValid(problem)
+		            println(stderr, "Problem ", probNum, " is invalid!")
+		            continue
+		        end
 
-				# if instance.modelResults.fullModel !== nothing
-				#	continue
-				# end
+		        # if instance.modelResults.fullModel !== nothing
+		        #	continue
+		        # end
 
-				# res = runLinear(problem, ORDER_FIRST_STRICT, SHARED_EVENTS, timeLimit=60 * 60, startSolution=true)
-				# instance.modelResults.fullModel = (solution = res[1], bound = res[2])
-				# if !ismissing(res[1])
-				#	instance.modelResults.fullModel = (solution = res[1], bound = res[2])
-				# end
+		        # res = runLinear(problem, ORDER_FIRST_STRICT, SHARED_EVENTS, timeLimit=60 * 60, startSolution=true)
+		        # instance.modelResults.fullModel = (solution = res[1], bound = res[2])
+		        # if !ismissing(res[1])
+		        #	instance.modelResults.fullModel = (solution = res[1], bound = res[2])
+		        # end
 
-				# res = runLinear(problem, ORDER_FIRST_STRICT, BUFFER_ONLY, timeLimit=60 * 60)
-				# instance.modelResults.bufferOnly = (solution = res[1], bound = res[2])
+		        # res = runLinear(problem, ORDER_FIRST_STRICT, BUFFER_ONLY, timeLimit=60 * 60)
+		        # instance.modelResults.bufferOnly = (solution = res[1], bound = res[2])
 
-				# res = runLinear(problem, ASSIGNMENT_ONLY_SHARED, NO_CARS, timeLimit=60 * 60)
-				# instance.modelResults.assignmentOnly = (solution = res[1], bound = res[2])
+		        # res = runLinear(problem, ASSIGNMENT_ONLY_SHARED, NO_CARS, timeLimit=60 * 60)
+		        # instance.modelResults.assignmentOnly = (solution = res[1], bound = res[2])
 
-				samp = EncodingSample{PermutationEncoding}(problem.jobCount, problem.machineCount)
-				sf(jobs) = computeTimeLazyReturn(jobs, problem, Val(false), true)
-				starts = rand(samp, 10)
-				# goodStarts = [PermutationEncoding(likehoodBased(jobDistance(problem.itemsNeeded), i)) for i = 1:problem.jobCount]
-				# bestInd = argmin(map(sf, goodStarts))
-				# bestStart = goodStarts[bestInd]
-				# starts = fill(bestStart, 10)
+		        samp = EncodingSample{PermutationEncoding}(problem.jobCount, problem.machineCount)
+		        sf(jobs) = computeTimeLazyReturn(jobs, problem, Val{false}(), true)
+		        starts = rand(samp, 10)
+		        # goodStarts = [PermutationEncoding(likehoodBased(jobDistance(problem.itemsNeeded), i)) for i = 1:problem.jobCount]
+		        # bestInd = argmin(map(sf, goodStarts))
+		        # bestStart = goodStarts[bestInd]
+		        # starts = fill(bestStart, 10)
 
-				dif = maxDif(starts[1], sf)
-				res = runAnnealing(problem, starts, 10^7, problem.jobCount^2, dif / 2, uniform=true, fast=false)
-				push!(instance.annealingResults, res)
+		        # dif = maxDif(starts[1], sf)
+		        # res = runAnnealing(problem, starts, 10^7, problem.jobCount^2, dif / 2, uniform=true, fast=false)
+		        # push!(instance.annealingResults, res)
 
-				res = runTabu(problem, starts, 1000, 3 * problem.jobCount, min(2 * problem.jobCount^2, 5000))
-				# res = runTabu(problem, starts, 2000, 600, 5000, distribution="item",fast=true, improvements=["itemBased","bestStart","fast"])
-				push!(instance.tabuResults, res)
-			end
-			GC.gc()
+		        # res = runTabu(problem, starts, 1000, 3 * problem.jobCount, min(2 * problem.jobCount^2, 5000))
+		        # res = runTabu(problem, starts, 2000, 600, 5000, distribution="item",fast=true, improvements=["itemBased","bestStart","fast"])
+		        # push!(instance.tabuResults, res)
+
+		        # res = runHybrid1(problem, starts, 200, 100, 10, 89 * 3, 2000, 500.0, 0.99999)
+		        # push!(instance.otherResults, res)
+
+		        res = runHybrid2(problem, starts, 200, 200, 10, 89 * 3,2000,20)
+		        push!(instance.otherResults, res)
+		    end
+		    GC.gc()
 		end
 	finally
 		open(resFile, "w") do file
-			toJson(file, results);
-		end;
+			toJson(file, results)
+		end
 	end
 end
 GC.gc()
-=#
+
 
 # let
 # 	instance = createInstance(20,4,['A'],missing,6,30,6)
@@ -114,7 +125,7 @@ GC.gc()
 # 	println(solution.score,' ',res.time)
 # end
 
-
+#=
 probSize = 200
 probNum = 6
 machineCount = 4
@@ -164,7 +175,7 @@ println("$time1 $time4 $time5")
 df=CSV.File("exp/times.tsv")|>DataFrame
 push!(df,(problem.jobCount,gethostname(),time0,time2,time3,time1,time4,time5))
 CSV.write("exp/times.tsv",df,delim='\t')
-
+=#
 #=
 let
 	results = fromJson(Vector{ProblemInstance}, JSON.parsefile("exp/results.json"))
