2392ecf050adc5c33beda623e65e919e20c764d8
Vedblade <18503283+yad439@users.noreply.github.com>
2021-12-12 22:22:16 +0700

    Experiment: partial hybrid 1+3

--- src/runner.jl
+++ src/runner.jl
@@ -28,7 +28,8 @@ let
 	# experiments = [3, 13, 6, 7, 9, 16, 17, 19, 5, 15, 4, 14, 48, 49, 8, 18, 2, 12, 1, 11, 25, 22, 26, 23, 27, 21, 47, 24, 20, 45, 46, 33, 39, 38, 32, 37, 31, 30, 43, 44]
 	try
 		# for probNum = [4] # [1, 4, 8] [2, 7, 10]
-		for (ind, val) in Iterators.enumerate(experiments)
+		exps=collect(Iterators.enumerate(experiments))
+		for (ind, val) in exps[26:end]
 			println("Instance ", ind)
 			let
 				# bufferSize = problemStats(probSize, probNum, ['A']).maxItems
