ba6ce5995e43c4f7a633c14d64d53421d346ab54
Vedblade <18503283+yad439@users.noreply.github.com>
2022-05-19 21:54:56 +0700

    Implement taking multiple solution in annealing

--- src/annealing.jl
+++ src/annealing.jl
@@ -2,112 +2,121 @@ include("encodings.jl")
 include("utility.jl")
 
 struct AnnealingSettings
-	searchTries::Int
-	isDynamic::Bool
-	sameTemperatureTries::Int
-	startTheshold::Float64
-	decreasingFunction::FuncR{Float64}
-	applyChange::FuncR{Bool}
+    searchTries::Int
+    neightborhoodSize::Int
+    isDynamic::Bool
+    sameTemperatureTries::Int
+    startTheshold::Float64
+    decreasingFunction::FuncR{Float64}
+    applyChange::FuncR{Bool}
 end
 struct AnnealingSettings2
-	searchTries::Int
-	isDynamic::Bool
-	sameTemperatureTries::Int
-	startTheshold::Float64
-	decreasingFunction::Function
-	applyChange::Function
-	changeGenerator::Function
+    searchTries::Int
+    isDynamic::Bool
+    sameTemperatureTries::Int
+    startTheshold::Float64
+    decreasingFunction::Function
+    applyChange::Function
+    changeGenerator::Function
 end
 
 function modularAnnealing(settings::AnnealingSettings, scoreFunction::F, startTimeTable) where {F}
-	timeTable = startTimeTable
-	minval = scoreFunction(timeTable)
-	minsol = copy(timeTable)
-	counter = 0
-	threshold = settings.startTheshold
+    timeTable = startTimeTable
+    minval = scoreFunction(timeTable)
+    minsol = copy(timeTable)
+    counter = 0
+    threshold = settings.startTheshold
 
-	prevScore = minval
-	scounter = 1
-	while counter < settings.searchTries
-		newChange, restoreChange = randomChange!(timeTable, change -> true)
-		score = scoreFunction(timeTable)
-		if settings.applyChange(prevScore, score, threshold)
-			prevScore = score
-		else
-			change!(timeTable, restoreChange)
-		end
-		if score < minval
-			if settings.isDynamic
-				counter = 0
-			else
-				counter += 1
-			end
-			minval = score
-			copy!(minsol, timeTable)
-		else
-			counter += 1
-		end
-		if scounter ≥ settings.sameTemperatureTries
-			threshold = settings.decreasingFunction(threshold)
-			scounter = 1
-		else
-			scounter += 1
-		end
-	end
-	(score = minval, solution = minsol)
+    prevScore = minval
+    scounter = 1
+    while counter < settings.searchTries
+        iterMin = typemax(Int)
+        toApply = (defaultChange(timeTable), 0, 0)
+        for _ = 1:settings.neightborhoodSize
+            newChange, restoreChange = randomChange!(timeTable, change -> true)
+            score = scoreFunction(timeTable)
+            change!(timeTable, restoreChange)
+            if score < iterMin
+                iterMin = score
+                toApply = newChange
+            end
+        end
+        if settings.applyChange(prevScore, iterMin, threshold)
+            prevScore = iterMin
+            change!(timeTable, toApply)
+        end
+        if iterMin < minval
+            if settings.isDynamic
+                counter = 0
+            else
+                counter += 1
+            end
+            minval = iterMin
+            copy!(minsol, timeTable)
+        else
+            counter += 1
+        end
+        if scounter ≥ settings.sameTemperatureTries
+            threshold = settings.decreasingFunction(threshold)
+            scounter = 1
+        else
+            scounter += 1
+        end
+    end
+    (score=minval, solution=minsol)
 end
 
 function modularAnnealing(settings::AnnealingSettings2, scoreFunction::F, startTimeTable) where {F}
-	timeTable = startTimeTable
-	minval = scoreFunction(timeTable)
-	minsol = copy(timeTable)
-	counter = 0
-	threshold = settings.startTheshold
+    timeTable = startTimeTable
+    minval = scoreFunction(timeTable)
+    minsol = copy(timeTable)
+    counter = 0
+    threshold = settings.startTheshold
 
-	prevScore = minval
-	scounter = 1
-	while counter < settings.searchTries
-		newChange = settings.changeGenerator(timeTable)
-		restoreChange = change!(timeTable, newChange)
-		score = scoreFunction(timeTable)
-		if settings.applyChange(prevScore, score, threshold)
-			prevScore = score
-		else
-			change!(timeTable, restoreChange)
-		end
-		if score < minval
-			if settings.isDynamic
-				counter = 0
-			else
-				counter += 1
-			end
-			minval = score
-			copy!(minsol, timeTable)
-		else
-			counter += 1
-		end
-		if scounter ≥ settings.sameTemperatureTries
-			threshold = settings.decreasingFunction(threshold)
-			scounter = 1
-		else
-			scounter += 1
-		end
-	end
-	(score = minval, solution = minsol)
+    prevScore = minval
+    scounter = 1
+    while counter < settings.searchTries
+        newChange = settings.changeGenerator(timeTable)
+        restoreChange = change!(timeTable, newChange)
+        score = scoreFunction(timeTable)
+        if settings.applyChange(prevScore, score, threshold)
+            prevScore = score
+        else
+            change!(timeTable, restoreChange)
+        end
+        if score < minval
+            if settings.isDynamic
+                counter = 0
+            else
+                counter += 1
+            end
+            minval = score
+            copy!(minsol, timeTable)
+        else
+            counter += 1
+        end
+        if scounter ≥ settings.sameTemperatureTries
+            threshold = settings.decreasingFunction(threshold)
+            scounter = 1
+        else
+            scounter += 1
+        end
+    end
+    (score=minval, solution=minsol)
 end
 
 function maxDif(jobs::PermutationEncoding, scoreFunction)
-	minval = typemax(Int)
-	maxval = typemin(Int)
-	for type ∈ [PERMUTATION_MOVE, PERMUTATION_SWAP], arg1 in 1:length(jobs), arg2 in 1:length(jobs)
-		arg1 == arg2 && continue
-		restoreChange = change!(jobs, type, arg1, arg2)
-		score = scoreFunction(jobs)
-		change!(jobs, restoreChange)
-		score < minval && (minval = score)
-		score > maxval && (maxval = score)
-	end
-	maxval - minval
+    minval = typemax(Int)
+    maxval = typemin(Int)
+    for type ∈ [PERMUTATION_MOVE, PERMUTATION_SWAP], arg1 in 1:length(jobs), arg2 in 1:length(jobs)
+        arg1 == arg2 && continue
+        restoreChange = change!(jobs, type, arg1, arg2)
+        score = scoreFunction(jobs)
+        change!(jobs, restoreChange)
+        score < minval && (minval = score)
+        score > maxval && (maxval = score)
+    end
+    maxval - minval
 end
 
 #=function maxDif(jobs::TwoVectorEncoding, scoreFunction)
--- src/newTest.jl
+++ src/newTest.jl
@@ -19,5 +19,19 @@ result = differentialEvolution(settings,
     (c, t, po, pr) -> annealingReplacer(c, t, po, pr, (start=1000, endC=1e-5)))
 ##
 enc = PermutationEncoding(shuffle(1:instance.jobCount));
-sett = AnnealingSettings(2_000_000, false, 1, 1000, FuncR{Float64}(t -> t * (-1000 * log(10^-3))^(-1 / 2_000_000)), FuncR{Bool}((old, new, threshold) -> rand() < exp((old - new) / threshold)));
+sett = AnnealingSettings(1_000_000, 2, false, 1, 1000, FuncR{Float64}(t -> t * (-1000 * log(10^-3))^(-1 / 2_000_000)), FuncR{Bool}((old, new, threshold) -> rand() < exp((old - new) / threshold)));
 res2, _ = modularAnnealing(sett, p -> computeTimeLazyReturn(p.permutation, instance), enc)
+##
+df=DataFrame(iterCount=Int[],nsize=Int[],score=Int[]);
+lk=ReentrantLock();
+for iterCount ∈ [1_024, 16_384, 131_072, 1_048_576], nsize ∈ [1, 2, 4, 8]
+    @show iterCount nsize
+    sett = AnnealingSettings(iterCount ÷ nsize, nsize, false, 1, 1000, FuncR{Float64}(t -> t * (-1000 * log(10^-3))^(-1 / (iterCount ÷ nsize))), FuncR{Bool}((old, new, threshold) -> rand() < exp((old - new) / threshold)))
+    Threads.@threads for _ = 1:1
+        enc = PermutationEncoding(shuffle(1:instance.jobCount))
+        result, _ = modularAnnealing(sett, p -> computeTimeLazyReturn(p.permutation, instance), enc)
+        lock(lk) do
+            push!(df,(iterCount,nsize,result))
+        end
+    end
+end
