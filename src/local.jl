89dcd2b33b6dd4bc5e4bbfd9244e33b225185343
Vedblade <18503283+yad439@users.noreply.github.com>
2022-05-23 22:07:17 +0700

    Add fixed-iterations local search

--- src/local.jl
+++ src/local.jl
@@ -1,39 +1,59 @@
-using ProgressMeter
-using ValueHistories
-
 include("encodings.jl")
 
 struct LocalSearchSettings{T}
-	iterator::T
-	acceptFirst::Bool
+    iterator::T
+    acceptFirst::Bool
 end
 
-function modularLocalSearch(settings, scoreFunction, startTimeTable, showProgress = true)
-	progress = ProgressUnknown("Local search:")
+struct LocalSearchSettings2
+    iterationCount::Int
+    acceptFirst::Bool
+end
 
-	timeTable = startTimeTable
-	score = scoreFunction(timeTable)
+function modularLocalSearch(settings::LocalSearchSettings, scoreFunction, startTimeTable)
+    timeTable = startTimeTable
+    score = scoreFunction(timeTable)
 
-	history = QHistory(typeof(score))
-	push!(history, score)
-	while true
-		minScore = score
-		minChange = (0, 0, 0)
-		for change ∈ settings.iterator
-			restore = change!(timeTable, change)
-			val = scoreFunction(timeTable)
-			if val < minScore
-				minScore = val
-				minChange = change
-				settings.acceptFirst && break
-			end
-			change!(timeTable, restore)
-		end
-		minScore == score && break
-		change!(timeTable, minChange)
-		score = minScore
-		push!(history, score)
-		showProgress && ProgressMeter.next!(progress, showvalues = (("Min score", score),))
-	end
-	(score = score, solution = timeTable, history = history)
+    while true
+        minScore = score
+        minChange = (0, 0, 0)
+        for change ∈ settings.iterator
+            restore = change!(timeTable, change)
+            val = scoreFunction(timeTable)
+            if val < minScore
+                minScore = val
+                minChange = change
+                settings.acceptFirst && break
+            end
+            change!(timeTable, restore)
+        end
+        minScore == score && break
+        change!(timeTable, minChange)
+        score = minScore
+    end
+    (score=score, solution=timeTable)
+end
+
+function modularLocalSearch(settings::LocalSearchSettings2, iterator, scoreFunction, startTimeTable)
+    timeTable = startTimeTable
+    score = scoreFunction(timeTable)
+
+    for _ = 1:settings.iterationCount
+        minScore = score
+        minChange = (0, 0, 0)
+        for change ∈ iterator()
+            restore = change!(timeTable, change)
+            val = scoreFunction(timeTable)
+            if val < minScore
+                minScore = val
+                minChange = change
+                settings.acceptFirst && break
+            end
+            change!(timeTable, restore)
+        end
+        minScore == score && break
+        change!(timeTable, minChange)
+        score = minScore
+    end
+    (score=score, solution=timeTable)
 end
