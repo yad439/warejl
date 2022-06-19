0286a7a2d2f11ca96a370b7dbc361acf71da315e
Vedblade <18503283+yad439@users.noreply.github.com>
2022-06-19 17:32:25 +0700

    Implement generational genetic algorithm

--- src/genetic.jl
+++ src/genetic.jl
@@ -11,6 +11,11 @@ struct GeneticSettings
     iterationLimit::Int
 end
 
+struct GeneticSettings2
+    iterationLimit::Int
+    generationSize::Int
+end
+
 struct DeEncoding
     data::Vector{Float64}
     score::Int
@@ -46,6 +51,21 @@ function stationaryGenetic(settings, scoreFunction, selector, crossover, mutator
         mutator(newSol)
         sₙ = PermEncoding(newSol, scoreFunction(newSol))
         replacer(population, s₁, s₂, sₙ)
+    end
+    minimum(sol.score for sol ∈ population)
+end
+
+function genrationalGenetic(settings, scoreFunction, selector, crossover, mutator, truncator, population)
+    candidates = Vector{eltype(population)}(undef, settings.generationSize)
+    for t = 1:settings.iterationLimit
+        for (i, (s₁, s₂)) ∈ enumerate(selector(population, length(candidates), t / settings.iterationLimit))
+            newSol = crossover(s₁, s₂)
+            mutator(newSol)
+            candidates[i] = PermEncoding(newSol, scoreFunction(newSol))
+        end
+        for (i, s) ∈ enumerate(truncator(population, candidates, t / settings.iterationLimit))
+            population[i] = s
+        end
     end
     minimum(sol.score for sol ∈ population)
 end
@@ -109,4 +129,34 @@ end
 function worstReplacer2(population, _, _, sol)
     population[argargmax(e -> e.score, population)] = sol
     nothing
+end
+
+extremeSelectorHelper(population, _, best) = best ? argmin(s -> s.score, population) : argmax(s -> s.score, population)
+
+batchSelectorAdaptor(selector, population, generationSize, progress) = (selector(population, progress) for _ = 1:generationSize)
+
+function bestTruncator(population, generationSize, _)
+    sorted = sort(population, by=s -> s.score)
+    view(sorted, 1:generationSize)
+end
+
+function truncatorExtAdapter(truncator, population, candidates, usePrevious, useBest, progress)
+    if usePrevious
+        wholePop = [population; candidates]
+        if useBest
+            best = argmin(s -> s.score, wholePop)
+            rest = truncator(wholePop, length(population) - 1, progress)
+            return push!(collect(rest), best)
+        else
+            return truncator(wholePop, length(population), progress)
+        end
+    else
+        if useBest
+            best = argmin(s -> s.score, population)
+            rest = truncator(candidates, length(population) - 1, progress)
+            return push!(collect(rest), best)
+        else
+            return truncator(candidates, length(population), progress)
+        end
+    end
 end
